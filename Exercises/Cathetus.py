56cbe14867390f132d4caed961a72f6d41f65a50
1joan <joan17920@gmail.com>
2025-09-16 12:45:50 +0200

    dele

--- Exercises/Cathetus.py
+++
@@ -1,8 +0,0 @@
-import math
-def cathetus (a, c):
-  if a >= c:
-    print ("Hypotus must > cathetus ")
-  else:
-   return math.sqrt (c**2 - a**2)
-
-print (cathetus(5, 7))
--- Exercises/hypot.py
+++
@@ -1,4 +0,0 @@
-import math
-a = 3; b =4
-c = math.hypot(a, b)
-print (c)
